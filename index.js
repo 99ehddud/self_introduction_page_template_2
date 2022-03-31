d16a33503212bce0e01460d5d748f80f023a37d8
99ehddud <99ehddud@gmail.com>
2022-03-31 22:40:34 +0900

    Ver.x.0.3 (Add Img/Paragraph & Modify Function: resize())

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function resize() {
 
     const main = document.querySelector('main');
     const front = document.querySelector('#front');
+    const imageDiv = document.querySelector('#image_div');
+    const picture = document.querySelector('#picture');
+    const informationDiv = document.querySelector('#information_div');
     const back = document.querySelector('#back');
     const footer = document.querySelector('footer');
 
@@ -13,6 +16,12 @@ function resize() {
     main.style.height = (innerHeight-footerHeight) + 'px';
     front.style.width = (innerWidth * 0.6) + 'px'; // need to be modified
     front.style.height = (innerHeight * 0.6) + 'px'; // need to be modified
+    imageDiv.style.width = (front.offsetWidth / 2 ) + 'px';
+    imageDiv.style.height = front.offsetHeight + 'px';
+    picture.style.width = (imageDiv.offsetWidth / 1.7) + 'px';
+    picture.style.height = (picture.offsetWidth * 4 / 3) + 'px';
+    informationDiv.style.width = (front.offsetWidth / 2) + 'px';
+    informationDiv.style.height = front.offsetHeight + 'px';
     footer.style.width = innerWidth + 'px';
 }
 
