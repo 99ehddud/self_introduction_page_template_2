2419c712ab67970d64d9786b0d2c7470e48e5a0c
99ehddud <99ehddud@gmail.com>
2022-04-07 21:49:09 +0900

    Ver.x.0.2 (Restructuring)

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ function resize() {
     let innerHeight = window.innerHeight;
 
     const main = document.querySelector('main');
+    const front = document.querySelector('#front');
+    const back = document.querySelector('#back');
     
     main.style.width = innerWidth + 'px';
     main.style.height = innerHeight + 'px';
@@ -15,5 +17,21 @@ function resize() {
 // should rearrange image and paragraph
 
 function rotateToBack() {
+    const front = document.querySelector('#front');
+    const back = document.querySelector('#back');
+
+    front.style.transform = 'rotateY(180deg)';
+    front.style.zIndex = 0;
+    back.style.transform = 'rotateY(360deg)';
+    back.style.zIndex = 1;
+}
+
+function rotateToFront() {
+    const front = document.querySelector('#front');
+    const back = document.querySelector('#back');
     
+    front.style.transform = 'rotateY(360deg)';
+    front.style.zIndex = 1;
+    back.style.transform = 'rotateY(180deg)';
+    back.style.zIndex = 0;
 }
