89ec80ae7c0a451948950991c21c92256aab382e
99ehddud <99ehddud@gmail.com>
2022-04-09 22:06:26 +0900

    Ver.x.0.4 (Modify Function: resize())

--- index.js
+++ index.js
@@ -10,6 +10,27 @@ function resize() {
     
     main.style.width = innerWidth + 'px';
     main.style.height = innerHeight + 'px';
+
+    let frontWidth = front.offsetWidth;
+    let frontHeight = front.offsetHeight;
+
+    let left = (innerWidth - frontWidth) / 2;
+    let top = (innerHeight - frontHeight) / 2;
+
+    if (innerWidth-frontWidth >= 0) {
+        front.style.left = left + 'px';
+        back.style.left = left + 'px';
+    } else {
+        front.style.left = '0px';
+        back.style.left = '0px';
+    }
+    if (innerHeight-frontHeight >= 0) {
+        front.style.top = top + 'px';
+        back.style.top = top + 'px';
+    } else {
+        front.style.top = '0px';
+        back.style.top = '0px';
+    }
 }
 
 
@@ -19,11 +40,13 @@ function resize() {
 function rotateToBack() {
     const front = document.querySelector('#front');
     const back = document.querySelector('#back');
-
+    
     front.style.transform = 'rotateY(180deg)';
     front.style.zIndex = 0;
+    front.style.visibility = 'hidden';
     back.style.transform = 'rotateY(360deg)';
     back.style.zIndex = 1;
+    back.style.visibility = 'visible';
 }
 
 function rotateToFront() {
@@ -32,6 +55,8 @@ function rotateToFront() {
     
     front.style.transform = 'rotateY(360deg)';
     front.style.zIndex = 1;
+    front.style.visibility = 'visible';
     back.style.transform = 'rotateY(180deg)';
     back.style.zIndex = 0;
+    back.style.visibility = 'hidden';
 }
